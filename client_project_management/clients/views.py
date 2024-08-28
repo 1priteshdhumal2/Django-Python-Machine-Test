01ca5d00aa87df4487b5f3af939f8b3c4f48e2f4
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-08-28 20:35:33 +0530

    Created a view for listing all clients and creating a new client also created url for that

--- client_project_management/clients/views.py
+++ client_project_management/clients/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.authentication import TokenAuthentication
+from .models import Client, Project
+from .serializers import ClientSerializer, ProjectSerializer
 
 # Create your views here.
+class ClientListCreateView(generics.ListCreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    authentication_classes = [TokenAuthentication]
+    permissions_classes = [permissions.IsAuthenticated]
+    
