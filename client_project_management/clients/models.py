2cf5ff7ce6f1265724183992d46c07c1f0cc89b6
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-09-20 10:54:40 +0530

    small changes

--- client_project_management/clients/models.py
+++ client_project_management/clients/models.py
@@ -13,10 +13,10 @@ class Client(models.Model):
     
 class Project(models.Model):
     project_name = models.CharField(max_length=255)
-    client = models.ForeignKey(Client, related_name='projects', on_delete=models.CASCADE)
-    users = models.ManyToManyField(User, related_name="projects")
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, related_name='created_projects', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.project_name
